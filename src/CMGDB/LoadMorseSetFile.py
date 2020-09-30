0064dc02aaab7bffdd04e3e0b0ea07933c8a1169
Marcio Gameiro <marciogameiro@gmail.com>
2020-09-29 23:13:45 -0400

    Improve Morse set plotting

--- src/CMGDB/LoadMorseSetFile.py
+++ src/CMGDB/LoadMorseSetFile.py
@@ -2,9 +2,8 @@ import csv
 
 def LoadMorseSetFile(morse_set_fname):
     morse_sets = []
-    with open(morse_set_fname, 'r') as csvfile:
-        csvreader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC, skipinitialspace=True)
-        headers = next(csvreader)
-        for row in csvreader:
+    with open(morse_set_fname, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file, quoting=csv.QUOTE_NONNUMERIC, skipinitialspace=True)
+        for row in csv_reader:
             morse_sets.append(row)
     return morse_sets
--- src/CMGDB/PlotMorseSets.py
+++ src/CMGDB/PlotMorseSets.py
@@ -3,8 +3,8 @@ import matplotlib
 import matplotlib.pyplot as plt
 import CMGDB
 
-def PlotMorseSets(morse_sets, morse_nodes=None, proj_dims=None,
-                  cmap=matplotlib.cm.brg, fig_w=8, fig_h=8):
+def PlotMorseSets(morse_sets, morse_nodes=None, proj_dims=None, cmap=matplotlib.cm.brg,
+                  fig_w=8, fig_h=8, xlim=None, ylim=None):
     # Check if morse_sets is a Morse graph, file name, or list
     if type(morse_sets) == CMGDB._cmgdb.MorseGraph: # Morse graph
         morse_graph = morse_sets
@@ -20,10 +20,10 @@ def PlotMorseSets(morse_sets, morse_nodes=None, proj_dims=None,
         num_morse_sets = None
     # Plot Morse set boxes as a scatter plot
     PlotBoxesScatter(morse_sets, num_morse_sets=num_morse_sets, morse_nodes=morse_nodes,
-                     proj_dims=proj_dims, cmap=cmap, fig_w=fig_w, fig_h=fig_h)
+                     proj_dims=proj_dims, cmap=cmap, fig_w=fig_w, fig_h=fig_h, xlim=xlim, ylim=ylim)
 
-def PlotBoxesScatter(morse_sets, num_morse_sets=None, morse_nodes=None,
-                     proj_dims=None, cmap=matplotlib.cm.brg, fig_w=8, fig_h=8):
+def PlotBoxesScatter(morse_sets, num_morse_sets=None, morse_nodes=None, proj_dims=None,
+                     cmap=matplotlib.cm.brg, fig_w=8, fig_h=8, xlim=None, ylim=None):
     rect = morse_sets[0]
     assert len(rect) % 2 == 1, "Wrong dimension in Morse sets data"
     dim = int((len(rect) - 1) / 2)
@@ -45,18 +45,52 @@ def PlotBoxesScatter(morse_sets, num_morse_sets=None, morse_nodes=None,
         d1 = proj_dims[0]
         d2 = proj_dims[1]
     assert max(d1, d2) < dim, "Wrong projection dimensions"
-    # Get min and max x values
-    x_lower = [rect[d1] for rect in morse_sets if int(rect[-1]) in morse_nodes]
-    x_upper = [rect[dim + d1] for rect in morse_sets if int(rect[-1]) in morse_nodes]
-    x_axis_width = max(x_upper) - min(x_lower)
+    # Get min and max x and y values
+    if xlim == None:
+        x_min = min([rect[d1] for rect in morse_sets if int(rect[-1]) in morse_nodes])
+        x_max = max([rect[dim + d1] for rect in morse_sets if int(rect[-1]) in morse_nodes])
+        if x_max - x_min < 0.1:
+            x_min -= 0.05
+            x_max += 0.05
+    else:
+        x_min = xlim[0]
+        x_max = xlim[1]
+    if ylim == None:
+        y_min = min([rect[d2] for rect in morse_sets if int(rect[-1]) in morse_nodes])
+        y_max = max([rect[dim + d2] for rect in morse_sets if int(rect[-1]) in morse_nodes])
+        if y_max - y_min < 0.1:
+            y_min -= 0.05
+            y_max += 0.05
+    else:
+        y_min = ylim[0]
+        y_max = ylim[1]
+    x_axis_width = x_max - x_min
+    y_axis_height = y_max - y_min
     # Next plot Morse sets
     fig, ax = plt.subplots(figsize=(fig_w, fig_h), dpi=100)
+    # Set axis limits
+    ax.set_xlim([x_min, x_max])
+    ax.set_ylim([y_min, y_max])
     # Normalization for color map
     cmap_norm = matplotlib.colors.Normalize(vmin=0, vmax=num_morse_sets-1)
-    # Get unit size in data units for marker size
-    s0 = ((ax.get_window_extent().width / x_axis_width) * (72.0 / fig.dpi)) ** 2
+    # The scatter plot in Matplotlib uses a marker size in points**2 units.
+    # The relationship between points and pixels is 1 point = dpi/72 pixels,
+    # hence multiplying by 72 / fig.dpi gives the number of pixels in the plot.
+    # For example using size s=10**2 in a scatter plot will plot the markers
+    # with a size of 100 points^2, which changes size according to the dpi.
+    # On the other hand using size s=(10*72.0/fig.dpi)**2 will plot markers
+    # of size 100 pixels^2, which will be the same size regardless of the dpi.
+    # First we get unit size in data units, that is, the size in pixels that
+    # correspond to one unit of data (one unit in each axis). The value needs
+    # to be squared later for the scatter plot.
+    s0_x = (ax.get_window_extent().width / x_axis_width) * (72.0 / fig.dpi)
+    s0_y = (ax.get_window_extent().height / y_axis_height) * (72.0 / fig.dpi)
+    # Alternative way to set marker size in data units
+    # M = ax.transData.get_matrix()
+    # x_scale = M[0,0]
+    # y_scale = M[1,1]
     for morse_node in morse_nodes:
-        morse_set = [rect for rect in morse_sets if rect[-1] == morse_node]
+        morse_set = [rect for rect in morse_sets if int(rect[-1]) == morse_node]
         # Use morse_node as color index for consistency if not plotting all
         clr = matplotlib.colors.to_hex(cmap(cmap_norm(morse_node)))
         X = []; Y = []; S = []
@@ -65,9 +99,17 @@ def PlotBoxesScatter(morse_sets, num_morse_sets=None, morse_nodes=None,
             p2 = [rect[dim + d1], rect[dim + d2]]       # Upper point
             p = list((np.array(p1) + np.array(p2)) / 2) # Center point
             s = list((np.array(p2) - np.array(p1)))     # Rect size
+            s_x = (s0_x * s[0]) ** 2 # Scatter x-axis size
+            s_y = (s0_y * s[1]) ** 2 # Scatter y-axis size
+            # Alternative way to set marker size in data units
+            # s_x = (x_scale * s[0]) ** 2 # Scatter x-axis size
+            # s_y = (y_scale * s[1]) ** 2 # Scatter y-axis size
             X.append(p[0])
             Y.append(p[1])
-            S.append(s0 * max(s) ** 2)
+            # Use max of both sizes
+            S.append(max(s_x, s_y))
         plt.scatter(X, Y, s=S, marker='s', c=clr)
         # plt.scatter(X, Y, s=S, marker='s', c=clr, alpha=0.5)
+    # ax.set_aspect('equal')
+    # plt.grid(True)
     plt.show()
